6ad9e78e573f7fd8cbad80c18afc010f19e6e212
Mateusz <mikula@onet.com.pl>
2020-10-01 22:43:20 +0200

    Create Project

---
+++ src/main/java/com/polsource/api/crudassignemnt/CrudassignemntApplication.java
@@ -0,0 +1,13 @@
+package com.polsource.api.crudassignemnt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudassignemntApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudassignemntApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/polsource/api/crudassignemnt/CrudassignemntApplicationTests.java
@@ -0,0 +1,13 @@
+package com.polsource.api.crudassignemnt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudassignemntApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
